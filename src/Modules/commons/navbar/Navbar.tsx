8d27c928a37809b13b8c9be92c14dc4a740f117e
codechappie <davidproyecto01@gmail.com>
2021-07-31 16:32:33 -0500

    responsive menu updated

--- src/Modules/commons/navbar/Navbar.tsx
+++ src/Modules/commons/navbar/Navbar.tsx
@@ -1,6 +1,18 @@
 import { Link } from "react-router-dom";
 import style from './navbar.module.scss';
+
+import facebookIcon from '../../../assets/icons/facebook-icon.svg'
+import instagramIcon from '../../../assets/icons/instagram-icon.svg'
+import { useState } from "react";
+
 const Navbar = () => {
+    const [showResponsiveMenu, setShowResponsiveMenu] = useState(false)
+
+    const toggleMenu = () => {
+        console.log("clieckd")
+        setShowResponsiveMenu(!showResponsiveMenu)
+    }
+
     return (
         <nav className={style.navbar}>
             <div className={style.navbar__container}>
@@ -12,7 +24,7 @@ const Navbar = () => {
                     <Link to="/" className={style.item}>Packages</Link>
                     <Link to="/" className={style.item}>+1 817 210 6443</Link>
                     <Link className={style.button__planning__trip} to="/">Start Planning My Trip</Link>
-                    <div className={style.menu__btn}>
+                    <div className={style.menu__btn} onClick={toggleMenu}>
                         <span>Menu</span>
                         <div className={style.menu__bars}>
                             <span className={style.bar}></span>
@@ -21,6 +33,26 @@ const Navbar = () => {
                         </div>
                     </div>
                 </div>
+
+                <div className={`${style.responsive__menu} ${showResponsiveMenu ? `${style.show}` : ''}`}>
+                    <div className={style.close__btn} onClick={toggleMenu}>✖</div>
+                    <Link to="/" className={style.item}>Our story</Link>
+                    <Link to="/" className={style.item}>Specialists</Link>
+                    <Link to="/" className={style.item}>Why us</Link>
+                    <Link to="/" className={style.item}>Our story</Link>
+                    <Link to="/" className={style.item}>Our staff</Link>
+                    <Link to="/" className={style.item}>+1 817 210 6443</Link>
+                    <Link className={style.button__planning__trip} to="/">Start Planning My Trip</Link>
+                    <div className={style.networking}>
+                        <a href="https://facebook.com" target="_blank" rel="noreferrer" className={style.icon}>
+                            <img src={facebookIcon} alt="" />
+                        </a>
+                        <a href="https://instagram.com" target="_blank" rel="noreferrer" className={style.icon}>
+                            <img src={instagramIcon} alt="" />
+                        </a>
+                    </div>
+                </div>
+
             </div>
         </nav>
     )
